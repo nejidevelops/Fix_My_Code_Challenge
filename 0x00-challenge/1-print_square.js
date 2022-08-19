ae0713f4a363e235301efda21ea4000da3eff791
Newton Ombese <newtonombese1@gmail.com>
2022-08-19 08:08:18 +0300

    fixng my javascript

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -5,18 +5,19 @@
     of the program.
 */
 
+
 if (process.argv.length <= 2) {
-  process.stderr.write('Missing argument\n');
-  process.stderr.write('Usage: ./1-print_square.js <size>\n');
-  process.stderr.write('Example: ./1-print_square.js 8\n');
-  process.exit(1);
+  process.stderr.write("Missing argument\n");
+  process.stderr.write("Usage: ./1-print_square.js <size>\n");
+  process.stderr.write("Example: ./1-print_square.js 8\n");
+  process.exit(1)
 }
 
-const size = parseInt(process.argv[2], 10);
+size = parseInt(process.argv[2], 10)
 
-for (let i = 0; i < size; i++) {
-  for (let j = 0; j < size; j++) {
-    process.stdout.write('#');
+for (var i = 0 ; i < size ; i ++) {
+  for (var j = 0 ; j < size ; j ++) {
+      process.stdout.write("#");
   }
-  process.stdout.write('\n');
+  process.stdout.write("\n");
 }
