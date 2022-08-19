5e8b2cffac2fc830e28f981e8f60860bf0a9d6fc
Newton Ombese <newtonombese1@gmail.com>
2022-08-19 08:00:05 +0300

    giving it a fix to my js file

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,17 +6,17 @@
 */
 
 if (process.argv.length <= 2) {
-    process.stderr.write('Missing argument\n');
-    process.stderr.write('Usage: ./1-print_square.js <size>\n');
-    process.stderr.write('Example: ./1-print_square.js 8\n');
-    process.exit(1);
+  process.stderr.write('Missing argument\n');
+  process.stderr.write('Usage: ./1-print_square.js <size>\n');
+  process.stderr.write('Example: ./1-print_square.js 8\n');
+  process.exit(1);
+}
+
+const size = parseInt(process.argv[2], 10);
+
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
+    process.stdout.write('#');
   }
-  
-  const size = parseInt(process.argv[2], 10);
-  
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      process.stdout.write('#');
-    }
-    process.stdout.write('\n');
-  }
+  process.stdout.write('\n');
+}
